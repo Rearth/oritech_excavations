d5f8040ef4784dbe31d62ad67dc46ffa98b65025
rearth <davidus2010@gmail.com>
2025-07-22 22:33:18 +0200

    Cleanup fabric files

--- fabric/src/main/java/rearth/excavations/fabric/ExcavationsFabric.java
+++ fabric/src/main/java/rearth/excavations/fabric/ExcavationsFabric.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import rearth.excavations.Excavation;
 
-public final class ExampleModFabric implements ModInitializer {
+public final class ExcavationsFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
--- fabric/src/main/java/rearth/excavations/fabric/client/ExcavationsClientFabric.java
+++ fabric/src/main/java/rearth/excavations/fabric/client/ExcavationsClientFabric.java
@@ -3,7 +3,7 @@ package rearth.excavations.fabric.client;
 import net.fabricmc.api.ClientModInitializer;
 import rearth.excavations.ExcavationClient;
 
-public final class ExampleModFabricClient implements ClientModInitializer {
+public final class ExcavationsClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
